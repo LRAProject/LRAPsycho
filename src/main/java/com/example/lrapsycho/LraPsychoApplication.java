394bcde043d91bea332acfe352921595ab3b35eb
Ági <raczagi07@gmail.com>
2023-12-27 16:44:58 +0100

    Initial commit

---
+++ src/main/java/com/example/lrapsycho/LraPsychoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lrapsycho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LraPsychoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LraPsychoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lrapsycho/LraPsychoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lrapsycho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LraPsychoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
